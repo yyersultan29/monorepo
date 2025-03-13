ce87311ce57f8973c6c11381e20ef6499a01c768
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-03-13 17:41:06 +0500

    done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { Button } from "@repo/ui/button/index";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
