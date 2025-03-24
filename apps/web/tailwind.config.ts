9857cc4369633dbf821b5743029ca7b00198f582
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-03-25 01:11:47 +0500

    wip

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,22 @@
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+// import sharedConfig from "@repo/tailwind-config";
 
 
-const config: Pick<Config, "content" | "presets" > = {
-  presets: [sharedConfig],
-  content: ["./app/**/*.tsx"],
+const config: Config = {
+  content: [  
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EF2A39"
+      }
+    }
+  },
+  
 };
 
 export default config;
