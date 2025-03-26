bcb185efd9983ce58db5c0ed44c9352cad126e97
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-03-26 10:57:02 +0500

    only admin changed

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -3,10 +3,9 @@ import { formatDate } from "@repo/utils";
 export default function Home() {
   const date = new Date().toISOString();
   return (
-    <div>
-      
-      <h2 className="bg-red-500 ">THIS IS ADMIN APP </h2>
-
+    <div className="w-full h-full flex justify-center items-center flex-col gap-3">
+    
+      <h2>THIS IS ADMIN APP </h2>
       <div>Current date: {formatDate(date)}</div>
     </div>
   );
